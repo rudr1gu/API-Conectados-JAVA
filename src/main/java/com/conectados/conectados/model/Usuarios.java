e63822fc5e92d0a723b3b805c00ea256fdae8402
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2024-12-07 11:42:14 -0300

    :tada: commit inicial

---
+++ src/main/java/com/conectados/conectados/model/Usuarios.java
@@ -0,0 +1,39 @@
+package com.conectados.conectados.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_usuarios")
+public class Usuarios {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String nome;
+
+    @NotBlank
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotBlank
+    private String senha;
+
+    @NotBlank
+    private String tipo;
+
+    private String foto;
+}
