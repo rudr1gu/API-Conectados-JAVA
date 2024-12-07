6ff9d8e8276913261b497a923c1e1fa88186fdf9
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2024-12-07 12:00:39 -0300

    :sparkles: feat: add model tag

---
+++ src/main/java/com/conectados/conectados/model/Tag.java
@@ -0,0 +1,24 @@
+package com.conectados.conectados.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_tags")
+public class Tag {
+        
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+    
+        @NotBlank
+        private String nome;
+}
