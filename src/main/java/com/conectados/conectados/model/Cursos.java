ad63ed2f6912447c01d558383d539ec20a24de3c
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2024-12-07 11:47:26 -0300

    :sparkles: feat: add model cursos

---
+++ src/main/java/com/conectados/conectados/model/Cursos.java
@@ -0,0 +1,30 @@
+package com.conectados.conectados.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_cursos")
+public class Cursos {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @NotBlank    
+    private String nome;
+    
+    @NotBlank
+    private int modulo;
+    
+    @NotBlank
+    private String periodo;
+}
