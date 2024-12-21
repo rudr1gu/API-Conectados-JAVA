e9c1cfac0ae1a040c3a54dbf0345ccc9bd2f2066
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2024-12-21 14:21:30 -0300

    :sparkles: feat: add relationships for Usuario and Materia in Curso model

--- src/main/java/com/conectados/conectados/model/Curso.java
+++ src/main/java/com/conectados/conectados/model/Curso.java
@@ -1,9 +1,16 @@
 package com.conectados.conectados.model;
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
@@ -27,4 +34,16 @@ public class Curso {
     
     @NotBlank
     private String periodo;
+
+    @OneToMany(mappedBy = "curso", cascade = CascadeType.ALL)
+    private List<Usuario> usuarios;
+
+    @ManyToMany
+    @JoinTable(
+        name = "cursos_materias",
+        joinColumns =  @JoinColumn(name = "curso_id"),
+        inverseJoinColumns = @JoinColumn(name = "materia_id")
+    )
+    private List<Materia> materias;
+
 }
