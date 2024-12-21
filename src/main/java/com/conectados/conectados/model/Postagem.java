fa6f191d6d559bbd5984f10b314c707296f8c654
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2024-12-21 14:38:20 -0300

    :sparkles: feat: add relationships for Usuario, Curso, and Materia in Postagem model

--- src/main/java/com/conectados/conectados/model/Postagem.java
+++ src/main/java/com/conectados/conectados/model/Postagem.java
@@ -1,11 +1,15 @@
 package com.conectados.conectados.model;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
@@ -29,9 +33,21 @@ public class Postagem {
     @NotBlank
     private String conteudo;
 
+    @ManyToOne
+    private Usuario usuario;
+    
+    @ManyToOne
+    private Curso curso;
+
+    @ManyToOne
+    private Materia materia;
+
     @CreationTimestamp
     private LocalDate dataCriacao;
 
     @UpdateTimestamp
     private LocalDate dataAtualizacao;
+
+    @OneToMany(mappedBy = "postagem", cascade = CascadeType.ALL)
+    private List<Resposta> respostas;
 }
