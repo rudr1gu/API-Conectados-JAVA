5c285dfff76725f33a3278f2e4730e5ca69cabef
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2024-12-21 14:28:56 -0300

    :sparkles: feat: add relationships for Curso, Tag, and Postagem in Materia model

--- src/main/java/com/conectados/conectados/model/Materia.java
+++ src/main/java/com/conectados/conectados/model/Materia.java
@@ -1,10 +1,17 @@
 package com.conectados.conectados.model;
 
 
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
@@ -22,4 +29,18 @@ public class Materia {
 
     @NotBlank
     private String nome;
+
+    @ManyToMany(mappedBy = "materias")
+    private List<Curso> cursos;
+
+    @ManyToMany
+    @JoinTable(
+        name = "materias_tags",
+        joinColumns = @JoinColumn(name = "materia_id"),
+        inverseJoinColumns = @JoinColumn(name = "tag_id")
+    )
+    private List<Tag> tags;
+
+    @OneToMany(mappedBy = "materia", cascade = CascadeType.ALL)
+    private List<Postagem> postagens;
 }
