988bcdb7c927afae12f00ce41258a0bece8b6801
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2024-12-21 13:30:41 -0300

    :sparkles: feat: add Resposta model with fields for content and timestamps

---
+++ src/main/java/com/conectados/conectados/model/Resposta.java
@@ -0,0 +1,30 @@
+package com.conectados.conectados.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "tb_respostas")
+public class Resposta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String conteudo;
+
+    private LocalDate dataCriacao;
+
+    private LocalDate dataAtualizacao;
+}
