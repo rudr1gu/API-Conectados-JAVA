7ad1e3a8e1a5f5a22856462dc838c5e452658cc7
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2024-12-21 14:40:16 -0300

    :sparkles: feat: add relationships for Usuario and Postagem in Resposta model

--- src/main/java/com/conectados/conectados/model/Resposta.java
+++ src/main/java/com/conectados/conectados/model/Resposta.java
@@ -1,11 +1,15 @@
 package com.conectados.conectados.model;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
@@ -27,4 +31,13 @@ public class Resposta {
     private LocalDate dataCriacao;
 
     private LocalDate dataAtualizacao;
+
+    @ManyToOne
+    private Usuario usuario;
+
+    @ManyToOne
+    private Postagem postagem;
+
+    @OneToMany(mappedBy = "resposta", cascade = CascadeType.ALL)
+    private List<Estrela> estrelas;
 }
