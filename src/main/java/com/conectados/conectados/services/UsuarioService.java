eee0a7a6bc2a7dd0e93eaa6cb912fe9fe4972628
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-04-07 19:50:59 -0300

    :sparkles: feat: enhance UsuarioService with user authentication and update methods for user details

--- src/main/java/com/conectados/conectados/services/UsuarioService.java
+++ src/main/java/com/conectados/conectados/services/UsuarioService.java
@@ -3,14 +3,16 @@ package com.conectados.conectados.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties.Jwt;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.conectados.conectados.domain.model.Usuario;
+import com.conectados.conectados.domain.model.UsuarioLogin;
 import com.conectados.conectados.domain.repository.UsuarioRepository;
 import com.conectados.conectados.security.JwtService;
 
@@ -27,7 +29,7 @@ public class UsuarioService {
     private AuthenticationManager authenticationManager;
 
     public Optional<Usuario> cadastrarUsuario(Usuario usuario) {
-        if(usuarioRepository.findByEmail(usuario.getEmail()).isPresent()) {
+        if (usuarioRepository.findByEmail(usuario.getEmail()).isPresent()) {
             return Optional.empty();
         }
         usuario.setSenha(criptografarSenha(usuario.getSenha()));
@@ -36,26 +38,53 @@ public class UsuarioService {
     }
 
     public Optional<Usuario> atualizarUsuario(Usuario usuario) {
-        if(usuarioRepository.findById(usuario.getId()).isPresent()){
+        if (usuarioRepository.findById(usuario.getId()).isPresent()) {
             Optional<Usuario> buscaUsuario = usuarioRepository.findByEmail(usuario.getEmail());
 
-            if((buscaUsuario.isPresent()) && (buscaUsuario.get().getId() != usuario.getId())) {
+            if ((buscaUsuario.isPresent()) && (buscaUsuario.get().getId() != usuario.getId())) {
                 throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Usuário já existe!", null);
             }
-           
+
             usuario.setSenha(criptografarSenha(usuario.getSenha()));
 
             return Optional.of(usuarioRepository.save(usuario));
         }
 
         return Optional.empty();
-    }    
+    }
 
+    public Optional<UsuarioLogin> autenticarUsuario(Optional<UsuarioLogin> usuarioLogin) {
 
+        var credenciais = new UsernamePasswordAuthenticationToken(usuarioLogin.get().getEmail(),
+                usuarioLogin.get().getSenha());
+
+        Authentication authentication = authenticationManager.authenticate(credenciais);
+
+        if (authentication.isAuthenticated()) {
+
+            Optional<Usuario> usuario = usuarioRepository.findByEmail(usuarioLogin.get().getEmail());
+
+            if (usuario.isPresent()) {
+                usuarioLogin.get().setId(usuario.get().getId());
+                usuarioLogin.get().setNome(usuario.get().getNome());
+                usuarioLogin.get().setFoto(usuario.get().getFoto());
+                usuarioLogin.get().setTipo(usuario.get().getTipo());
+                usuarioLogin.get().setToken(gerarToken(usuario.get().getEmail()));
+                usuarioLogin.get().setSenha("");
+
+                return usuarioLogin;
+            }
+        }
+
+        return Optional.empty();
+    }
 
     private String criptografarSenha(String senha) {
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		return encoder.encode(senha);
-	}
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        return encoder.encode(senha);
+    }
 
+    private String gerarToken(String email) {
+        return "Bearer " + jwtService.generateToken(email);
+    }
 }
