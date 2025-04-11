dc84021a11b4012282d1234d01d631264f3612aa
Rodrigo Santos <rodrigo.santos.ii@hotmail.com>
2025-04-11 14:26:14 -0300

    :sparkles: feat: implement PostagemController with endpoints for retrieving and creating postagens

--- src/main/java/com/conectados/conectados/controller/PostagemController.java
+++ src/main/java/com/conectados/conectados/controller/PostagemController.java
@@ -1,5 +1,38 @@
 package com.conectados.conectados.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.conectados.conectados.domain.model.Postagem;
+import com.conectados.conectados.services.PostagemService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+
+@RestController
+@RequestMapping("/postagens")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class PostagemController {
 
+    @Autowired
+    private PostagemService postagemService;
+
+    @GetMapping("/all")
+    public ResponseEntity<List<Postagem>> getAllPostagens(){
+        return ResponseEntity.ok(postagemService.buscarTodasPostagens().orElse(null));
+    }
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<Postagem> cadastrarPostagem(@RequestBody Postagem postagem) {
+        return ResponseEntity.ok(postagemService.cadastrarPostagem(postagem));
+    }
+    
 }
